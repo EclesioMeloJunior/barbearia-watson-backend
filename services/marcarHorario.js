97198d063dc2713039f740fa3f6b0e041d3e5446
EclesioMeloJunior <eclesiomelo.1@gmail.com>
2019-10-11 11:35:36 -0400

    Validações de horario

--- services/marcarHorario.js
+++ services/marcarHorario.js
@@ -1,17 +1,27 @@
 const agendaModel = require("../models/agenda");
 
 const marcarHorarioService = async ({ servico, dia, horario, nome }) => {
+	const dataEHora = new Date(`${dia} ${horario}`);
+
+	const horarioOcupado = await agendaModel.find({ date: horario });
+
+	console.log(horarioOcupado);
+
+	if (horarioOcupado.length > 0) {
+		return {
+			message: `Este horário já está reservado, tente reservar outro horário.`
+		};
+	}
+
 	const agendaDto = {
 		servico,
-		date: new Date(`${dia} ${horario}`),
+		date: dataEHora,
 		pessoa: {
 			nome
 		}
 	};
 
 	const agenda = await agendaModel.create(agendaDto);
-
-	console.log(nome, agenda);
 
 	return {
 		message: `Horário marcado! Guarde seu código de reserva ${agenda._id}`
