c5aa82b8af8a3720b9b41ea54334198021d16634
EclesioMeloJunior <eclesiomelo.1@gmail.com>
2019-10-11 11:16:22 -0400

    Add name to payload and store

--- services/marcarHorario.js
+++ services/marcarHorario.js
@@ -1,16 +1,21 @@
 const agendaModel = require("../models/agenda");
 
-const marcarHorarioService = async ({ servico, dia, horario }) => {
+const marcarHorarioService = async ({ servico, dia, horario, nome }) => {
 	const agendaDto = {
 		servico,
-		date: new Date(`${dia} ${horario}`)
+		date: new Date(`${dia} ${horario}`),
+		pessoa: {
+			nome
+		}
 	};
 
 	const agenda = await agendaModel.create(agendaDto);
 
-	console.log(agenda);
+	console.log(name, agenda);
 
-	return { message: "Horário marcado com sucesso!" };
+	return {
+		message: `Horário marcado! Guarde seu código de reserva ${agenda._id}`
+	};
 };
 
 module.exports = marcarHorarioService;
