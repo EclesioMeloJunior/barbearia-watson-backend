5b22debba84da5bbdf363a140779c2aa03299159
EclesioMeloJunior <eclesiomelo.1@gmail.com>
2019-10-16 09:12:04 -0400

    alterando horario para formato 12 horas

--- services/marcarHorario.js
+++ services/marcarHorario.js
@@ -1,7 +1,7 @@
 const agendaModel = require("../models/agenda");
 
 const marcarHorarioService = async ({ servico, dia, horario, nome }) => {
-	const dataEHora = new Date(`${dia} ${horario}`);
+	const dataEHora = new Date(`${dia} ${tConvert(horario)}`);
 
 	const horarioOcupado = await agendaModel.find({ date: dataEHora });
 
@@ -25,5 +25,20 @@ const marcarHorarioService = async ({ servico, dia, horario, nome }) => {
 		message: `Horário marcado! Guarde seu código de reserva ${agenda._id}`
 	};
 };
+
+function tConvert(time) {
+	// Check correct time format and split into components
+	time = time.toString().match(/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [
+		time
+	];
+
+	if (time.length > 1) {
+		// If time format correct
+		time = time.slice(1); // Remove full string match value
+		time[5] = +time[0] < 12 ? "AM" : "PM"; // Set AM/PM
+		time[0] = +time[0] % 12 || 12; // Adjust hours
+	}
+	return time.join(""); // return adjusted time or original string
+}
 
 module.exports = marcarHorarioService;
